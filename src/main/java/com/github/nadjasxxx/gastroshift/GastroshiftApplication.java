bc7882ae0075a5cbb62f4b256e2efc6a6380109d
Nadja Sporn <312873207+NadjaSxxx@users.noreply.github.com>
2026-08-04 12:43:37 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/github/nadjasxxx/gastroshift/GastroshiftApplication.java
@@ -0,0 +1,13 @@
+package com.github.nadjasxxx.gastroshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nadjasxxx/gastroshift/GastroshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nadjasxxx.gastroshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
